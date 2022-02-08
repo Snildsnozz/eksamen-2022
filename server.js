fab21f79639e2bf127b1ea549e982d146070de1b
Sille <sille@gmail.com>
2022-02-08 12:16:16 +0100

    joke nu virker test

--- server.js
+++ server.js
@@ -1,153 +1,161 @@
-const express = require('express');
-const app = express(); //gør vi kan kalde metoderne (get, put osv). 
-const port = 3000; 
-const cors = require('cors')
-const fs = require('fs');
+const express = require("express");
+const app = express(); //gør vi kan kalde metoderne (get, put osv).
+const port = 3000;
+const cors = require("cors");
+const fs = require("fs");
 const router = express.Router();
 
-app.use(cors())
-app.use(express.json()) 
-app.use(express.static('./views'));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./views"));
 app.use("/", router);
-
 
 //app.use('/users', userController);
 
 app.listen(port, () => {
-    console.log(`Den rigtige server lytter på http://localhost:${port}`)
-})
-
-
-
-app.post('/produktnavnarray', (req, res) => {
-
-    let dataArray = JSON.parse(fs.readFileSync('data/produktnavn.json'))
-
-    dataArray.push(req.body)   //nu hvor det er et array kan man bruge .push. husk tomt array i json filen så den kan bruge array funktionerne. 
-
-    fs.writeFile('data/produktnavn.json', JSON.stringify(dataArray, null, 4), err => {
-        if(err) res.send(err) //hvis der er en error, sender det error tilbage til brugeren. 
-            res.send({
-        msg: "Din vare er tilføjet"
-        }) //bliver det en succes sender den beskeden tilbage. 
-    })
-})
-
-app.get('/produktnavnarray', (req, res) => {
-    fs.readFile('data/produktnavn.json', function (err, data) {
-        //response.sendFile(path.join(__dirname + '/views/varer.html'));
-        if(err) res.send(err)
-        res.send(data)
-    })
-}); 
-
-
-app.put('/produktnavnarray', (req, res) => {
-
-    let dataArray = JSON.parse(fs.readFileSync('data/produktnavn.json'))
-
-    for (let i = 0; i < dataArray.length; i++) {
-
-        if(dataArray[i].id == req.body.id) {
-            dataArray[i].produkt = req.body.produkt
-            fs.writeFile('data/produktnavn.json', JSON.stringify(dataArray, null, 4), err => {
-                if(err) res.send(err)
-                    res.status(200).json({
-                msg: "Din vare er opdateret"
-                })
-            })
-        }
-    }
-})
-
-app.delete('/produktnavnarray/:id', (req, res) => {
-
-    let dataArray = JSON.parse(fs.readFileSync('data/produktnavn.json'))
-
-    for (let i = 0; i < dataArray.length; i++) {
-
-        if(dataArray[i].id == req.params.id) {
-            dataArray.splice(i, 1) //sletter et produkt af gangen. 
-
-            fs.writeFile('data/produktnavn.json', JSON.stringify(dataArray, null, 4), err => {
-                if(err) res.send(err)
-                    res.status(200).json({
-                msg: "Din vare er slettet"
-                })
-            })
-        }
-    }    
-})
-
-app.post('/opretBrugerId', (req, res) => {
-
-    let dataArray = JSON.parse(fs.readFileSync('data/user.json'))
-
-    dataArray.push(req.body)   //nu hvor det er et array kan man bruge .push. husk tomt array i json filen så den kan bruge array funktionerne. 
-
-    fs.writeFile('data/user.json', JSON.stringify(dataArray, null, 4), err => {
-        if(err) res.send(err) //hvis der er en error, sender det error tilbage til brugeren. 
-            res.send({
-        msg: "Din bruger er tilføjet"
-        }) //bliver det en succes sender den beskeden tilbage. 
-    })
-})
-
-app.put('/updateBrugerId', (req, res) => {
-
-    let dataArray = JSON.parse(fs.readFileSync('data/user.json'))
-
-    for (let i = 0; i < dataArray.length; i++) {
-
-        if(dataArray[i].id == req.body.id) {
-            dataArray[i].navn = req.body.navn
-            dataArray[i].kode = req.body.kode
-            dataArray[i].mail = req.body.mail
-            fs.writeFile('data/user.json', JSON.stringify(dataArray, null, 4), err => {
-                if(err) res.send(err)
-                    res.status(200).json({
-                msg: "Din bruger er opdateret"
-                })
-            })
-        }
-    }
-})
-
-app.delete('/sletBrugerId/:id', (req, res) => {
-
-    let dataArray = JSON.parse(fs.readFileSync('data/user.json'))
-
-    for (let i = 0; i < dataArray.length; i++) {
-
-        if(dataArray[i].id == req.params.id) {
-            dataArray.splice(i, 1) //sletter en bruger af gangen. 
-
-            fs.writeFile('data/user.json', JSON.stringify(dataArray, null, 4), err => {
-                if(err) res.send(err)
-                    res.status(200).json({
-                msg: "Din vare er slettet"
-                })
-            })
-        }
-    }    
-})
-
-router.post('/views/login', (req, res) => {
-    const user = {email: req.body.email, password: req.body.password}
-
-    let dataArray = JSON.parse(fs.readFileSync('data/user.json'))
-    let found = dataArray.find((u) => user.email == u.mail) 
-    //finder brugeren i databasen og tjekker om den stemmer med den opgivet mail. 
-
-    if (found) {
-        if(user.password == found.kode) {
-            res.status(200).send(found);
-        } else {
-            res.status(401).send(false);
-        }
-    } else {
-        res.status(404).send(false)
-    }
+  console.log(`Den rigtige server lytter på http://localhost:${port}`);
 });
 
-module.exports = app; 
+app.post("/produktnavnarray", (req, res) => {
+  let dataArray = JSON.parse(fs.readFileSync("data/produktnavn.json"));
+
+  dataArray.push(req.body);
+  //nu hvor det er et array kan man bruge .push. husk tomt array i json filen så den kan bruge array funktionerne.
+
+  fs.writeFile(
+    "data/produktnavn.json",
+    JSON.stringify(dataArray, null, 4),
+    (err) => {
+      if (err) res.send(err); //hvis der er en error, sender det error tilbage til brugeren.
+      res.send({
+        msg: "Din vare er tilføjet",
+        product: req.body,
+      }); //bliver det en succes sender den beskeden tilbage.
+    }
+  );
+});
+
+app.get("/produktnavnarray", (req, res) => {
+  fs.readFile("data/produktnavn.json", function (err, data) {
+    //response.sendFile(path.join(__dirname + '/views/varer.html'));
+    if (err) res.send(err);
+    res.send(data);
+  });
+});
+
+app.put("/produktnavnarray", (req, res) => {
+  let dataArray = JSON.parse(fs.readFileSync("data/produktnavn.json"));
+
+  for (let i = 0; i < dataArray.length; i++) {
+    if (dataArray[i].id == req.body.id) {
+      dataArray[i].produkt = req.body.produkt;
+      fs.writeFile(
+        "data/produktnavn.json",
+        JSON.stringify(dataArray, null, 4),
+        (err) => {
+          if (err) res.send(err);
+          res.status(200).json({
+            msg: "Din vare er opdateret",
+          });
+        }
+      );
+    }
+  }
+});
+
+app.delete("/produktnavnarray/:id", (req, res) => {
+  let dataArray = JSON.parse(fs.readFileSync("data/produktnavn.json"));
+
+  for (let i = 0; i < dataArray.length; i++) {
+    if (dataArray[i].id == req.params.id) {
+      dataArray.splice(i, 1); //sletter et produkt af gangen.
+
+      fs.writeFile(
+        "data/produktnavn.json",
+        JSON.stringify(dataArray, null, 4),
+        (err) => {
+          if (err) res.send(err);
+          res.status(200).json({
+            msg: "Din vare er slettet",
+          });
+        }
+      );
+    }
+  }
+});
+
+app.post("/opretBrugerId", (req, res) => {
+  let dataArray = JSON.parse(fs.readFileSync("data/user.json"));
+
+  dataArray.push(req.body); //nu hvor det er et array kan man bruge .push. husk tomt array i json filen så den kan bruge array funktionerne.
+
+  fs.writeFile("data/user.json", JSON.stringify(dataArray, null, 4), (err) => {
+    if (err) res.send(err); //hvis der er en error, sender det error tilbage til brugeren.
+    res.send({
+      msg: "Din bruger er tilføjet",
+    }); //bliver det en succes sender den beskeden tilbage.
+  });
+});
+
+app.put("/updateBrugerId", (req, res) => {
+  let dataArray = JSON.parse(fs.readFileSync("data/user.json"));
+
+  for (let i = 0; i < dataArray.length; i++) {
+    if (dataArray[i].id == req.body.id) {
+      dataArray[i].navn = req.body.navn;
+      dataArray[i].kode = req.body.kode;
+      dataArray[i].mail = req.body.mail;
+      fs.writeFile(
+        "data/user.json",
+        JSON.stringify(dataArray, null, 4),
+        (err) => {
+          if (err) res.send(err);
+          res.status(200).json({
+            msg: "Din bruger er opdateret",
+          });
+        }
+      );
+    }
+  }
+});
+
+app.delete("/sletBrugerId/:id", (req, res) => {
+  let dataArray = JSON.parse(fs.readFileSync("data/user.json"));
+
+  for (let i = 0; i < dataArray.length; i++) {
+    if (dataArray[i].id == req.params.id) {
+      dataArray.splice(i, 1); //sletter en bruger af gangen.
+
+      fs.writeFile(
+        "data/user.json",
+        JSON.stringify(dataArray, null, 4),
+        (err) => {
+          if (err) res.send(err);
+          res.status(200).json({
+            msg: "Din vare er slettet",
+          });
+        }
+      );
+    }
+  }
+});
+
+router.post("/views/login", (req, res) => {
+  const user = { email: req.body.email, password: req.body.password };
+
+  let dataArray = JSON.parse(fs.readFileSync("data/user.json"));
+  let found = dataArray.find((u) => user.email == u.mail);
+  //finder brugeren i databasen og tjekker om den stemmer med den opgivet mail.
+
+  if (found) {
+    if (user.password == found.kode) {
+      res.status(200).send(found);
+    } else {
+      res.status(401).send(false);
+    }
+  } else {
+    res.status(404).send(false);
+  }
+});
+
+module.exports = app;
---
+++ tests/test.js
@@ -0,0 +1,42 @@
+const chai = require("chai");
+const app = require("../server.js");
+const chaiHttp = require("chai-http");
+const expect = chai.expect;
+
+chai.should();
+chai.use(chaiHttp);
+
+describe(`POST /produktnavnarray`, () => {
+  it("should post a new product", (done) => {
+    chai
+      .request(app)
+      .post("/produktnavnarray")
+      .send({
+        id: "testid",
+        produkt: "sille 123",
+        price: "250",
+        category: "Møbler",
+      })
+      .end((err, res) => {
+        //tjek at fejlbeskeden ikke er sat
+        expect(err).to.be.null;
+
+        //tjek om statuskode er 200
+        expect(res.status).to.equal(200);
+
+        //Tjekker om msg er den rigtige
+        expect(res.body.msg).to.equal("Din vare er tilføjet");
+
+        //tjek om man får produkt i response.
+        expect(res.body.product).to.not.be.null;
+
+        //tjek om response er et objekt.
+        expect(res.body.product).to.be.an("object");
+
+        //tjek at response body ikke er et array
+        expect(res.body).to.not.be.an("array");
+
+        done();
+      });
+  });
+});
